e5263ec34aabbb37f849d402f337276ca06bb83b
pranavkale714 <96164632+pranavkale714@users.noreply.github.com>
2024-11-05 00:02:14 +0530

    Add files via upload

---
+++ data_types/src/data_types/types.java
@@ -0,0 +1,32 @@
+package data_types;
+
+public class types {
+
+	public static void main(String[] args) {
+		System.out.println("primitive");
+	    boolean a;//true and false
+	    Boolean A;
+		char b;
+		Character B; //IF FIRST LETTER IS CAPITAL MEANS IT IS CLASSES
+		int c;
+		Integer C;//this are INBUILDS CLASSES ALSO KNOWN AS WRAPPER CLASS
+		byte d;
+		short e;
+		long f;
+		Float G;
+		float g;
+		double h;//same as float //take more space than float
+		
+		System.out.println("non primitive or reference data type");
+		
+	    int n[];//array
+	    String j="jncjc";
+	    //3.CLASS
+	    //4.INTERFACE
+	    enum days{sunday,mondya,thusday};//cannot change (fixed)
+	    
+		
+
+	}
+
+}
