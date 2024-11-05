21ca6c7406f750f396a31eb0dce074bc715bea9e
pranavkale714 <96164632+pranavkale714@users.noreply.github.com>
2024-11-06 00:58:09 +0530

    Add files via upload

---
+++ default_values/src/default_values/default_val.java
@@ -0,0 +1,19 @@
+package default_values;
+
+public class default_val {
+	int a;
+	double b;
+	String c;
+	boolean d;
+
+	public static void main(String[] args) {
+		
+		default_val obj=new default_val();
+		System.out.println(obj.a);
+		System.out.println(obj.b);
+		System.out.println(obj.c);
+		System.out.println(obj.d);
+
+	}
+
+}
