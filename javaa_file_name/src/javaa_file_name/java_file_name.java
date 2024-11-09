7b0850ba592c3fb11c19aa7521f99f3669061087
pranavkale714 <96164632+pranavkale714@users.noreply.github.com>
2024-11-09 22:54:20 +0530

    Add files via upload

---
+++ javaa_file_name/src/javaa_file_name/java_file_name.java
@@ -0,0 +1,10 @@
+package javaa_file_name;
+
+public class java_file_name {
+
+	public static void main(String[] args) {
+		System.out.println("java file saved with .java");
+
+	}
+
+}
---
+++ javaa_file_name/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module javaa_file_name {
+}
