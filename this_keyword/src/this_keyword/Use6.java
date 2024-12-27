52978b127f7d6d542d00c780e96bfe3fa16d826b
pranavkale714 <96164632+pranavkale714@users.noreply.github.com>
2024-12-27 09:46:07 +0530

    Add files via upload

---
+++ this_keyword/src/this_keyword/Use6.java
@@ -0,0 +1,14 @@
+package this_keyword;
+
+//This keyword can be used to return the current class instance from the method
+public class Use6 {
+	Use6 m1()
+	{
+		return this;
+	}
+	public static void main(String[] args) {
+	Use6 t=new Use6();
+    
+	}
+
+}
