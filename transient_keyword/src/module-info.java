9a9b4de6dcf67ae2c2f6d6b1c1184a48a6821efa
pranavkale714 <96164632+pranavkale714@users.noreply.github.com>
2025-01-08 00:11:24 +0530

    Add files via upload

---
+++ transient_keyword/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module transient_keyword {
+}
---
+++ transient_keyword/src/transient_keyword/Theory.java
@@ -0,0 +1,10 @@
+package transient_keyword;
+
+public class Theory {
+
+	public static void main(String[] args) {
+		System.out.println("The transient keyword is used when we do not want the variable to be serialized, i.e., we do not want the value of that variable to be stored in a file.");
+		System.out.println("transient keyword plays an important role to meet security constraints.");
+	}
+
+}
