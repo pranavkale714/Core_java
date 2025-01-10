3ffe6fa9c2b93669edb2097802401e66e8dc6657
pranavkale714 <96164632+pranavkale714@users.noreply.github.com>
2025-01-10 11:36:37 +0530

    Create Tranasient_And_Static.java

---
+++ transient_keyword/src/transient_keyword/Tranasient_And_Static.java
@@ -0,0 +1,5 @@
+class Tranasient_And_Static {
+    public static void main(String[] args) {
+        System.out.println("transient and static : Since static fields are not part of state of the object, there is no use/impact of using transient keyword with static variables. However there is no compilation error.");
+    }
+}
