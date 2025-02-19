065ce1175bb9d3940cdf253a644797be89214a8c
pranavkale714 <96164632+pranavkale714@users.noreply.github.com>
2025-02-19 10:54:23 +0530

    Create Date_Class_2.java

---
+++ date_and_time/src/date_and_time/Date_Class_2.java
@@ -0,0 +1,19 @@
+package date_and_time;
+import java.util.Date;
+
+public class Date_Class_2 {
+
+	public static void main(String[] args) {
+		
+		//Depricated  methods so no longer is use in new versions
+		Date d=new Date();
+		System.out.println(d);
+		System.err.println(d.getDate());
+		System.err.println(d.getDay());
+		System.out.println(d.getHours());
+		System.out.println(d.getMonth());
+		
+
+	}
+
+}
