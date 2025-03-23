6b0743205ec058141d2bc131ee3f055687a9abe5
pranavkale714 <96164632+pranavkale714@users.noreply.github.com>
2025-03-23 23:44:58 +0530

    Add files via upload

---
+++ Wrapper_Class/src/Wrapper_class/Why_Wrapper_class.java
@@ -0,0 +1,16 @@
+package Wrapper_class;
+public class Why_Wrapper_class {
+
+	public static void main(String[] args) {
+		System.out.println("Wrapper classes provide a way to use primitive data types (int, boolean, etc..) as objects.");
+		System.out.println("for example when working with Collection objects, such as ArrayList, where primitive types cannot be used");
+		
+		System.out.println("Need of premitive Data Type:-");
+		
+		System.out.println("1.They convert primitive data types into objects. Objects are needed if we wish to modify the arguments passed into a method (because primitive types are passed by value");
+		System.out.println("2.The classes in java.util package handle only objects and hence wrapper classes help in this case");
+		System.out.println("3.Data structures in the Collection framework, such as ArrayList and Vector,store only objects (reference types) and not primitive types");
+		System.out.println("4.An object is needed to support synchronization in multithreading");
+	}
+
+}
