735b97fe38b962b533fa849942cd9aea0df09ee2
pranavkale714 <96164632+pranavkale714@users.noreply.github.com>
2025-03-27 09:36:01 +0530

    Add files via upload

---
+++ Strict_fp/src/Strict_fp/Theory.java
@@ -0,0 +1,11 @@
+package Strict_fp;
+public class Theory {
+
+	public static void main(String[] args) {
+		System.out.println("Java strictfp keyword ensures that you will get the same result on every platform if you perform operations in the floating-point variable");
+		System.out.println("The strictfp keyword can be applied on methods, classes and interfaces");
+		System.out.println("The strictfp keyword cannot be applied on abstract methods, variables or constructors");
+
+	}
+
+}
---
+++ Strict_fp/src/Strict_fp/demo.java
@@ -0,0 +1,10 @@
+package Strict_fp;
+
+ strictfp class demo{  //strictfp applied on class 
+	strictfp void m(){}//strictfp applied on method  
+	} 
+ strictfp interface M{}//strictfp applied on interface  
+
+ 
+ 
+
