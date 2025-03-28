3c52ba9d29e5f1ba549a5370e6b326516f7830bd
pranavkale714 <96164632+pranavkale714@users.noreply.github.com>
2025-03-28 09:26:06 +0530

    Add files via upload

---
+++ Command_Line_Args/src/Command_Line_Args/Command_Line_Args.java
@@ -0,0 +1,10 @@
+package Command_Line_Args;
+
+public class Command_Line_Args {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Command_Line_Args/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Command_Line_Args {
+}
