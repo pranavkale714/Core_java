097a5d3ada90718f996fbf6f7c189fb847080a6a
pranavkale714 <96164632+pranavkale714@users.noreply.github.com>
2025-03-29 23:19:03 +0530

    Update Command_Line_Args.java

--- Command_Line_Args/src/Command_Line_Args/Command_Line_Args.java
+++ Command_Line_Args/src/Command_Line_Args/Command_Line_Args.java
@@ -2,8 +2,17 @@ package Command_Line_Args;
 
 public class Command_Line_Args {
 
+	//  The Java command-line argument is an argument i.e. passed at the time of running the java program.	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// The file will comile by javac A 
+		// The file will be run by  java A sonoo jaiswal 1 3 abc  
+		// The words use after run command will be print after one another
+		
+		
+	    for(int i=0;i<args.length;i++)  
+	    System.out.println(args[i]);  
+	      
 
 	}
 
