4485018c05d9d5a4d4e8912ae30ca9cc65acaf0d
pranavkale714 <96164632+pranavkale714@users.noreply.github.com>
2025-04-01 23:31:20 +0530

    Add files via upload

---
+++ Regax_expression/src/Regax_expression/Demo.java
@@ -0,0 +1,18 @@
+package Regax_expression;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Demo {
+	//A regular expression is a sequence of characters that forms a search pattern. 
+	//When you search for data in a text, you can use this search pattern to describe what you are searching for.
+  public static void main(String[] args) {
+    Pattern pattern = Pattern.compile("w3schools", Pattern.CASE_INSENSITIVE);
+    Matcher matcher = pattern.matcher("Visit W3Schools!");
+    boolean matchFound = matcher.find();
+    if(matchFound) {
+      System.out.println("Match found");
+    } else {
+      System.out.println("Match not found");
+    }
+  }
+}
---
+++ Regax_expression/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Regax_expression {
+}
