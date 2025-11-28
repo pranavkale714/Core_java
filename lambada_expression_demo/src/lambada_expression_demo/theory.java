bef9b1917d00bce4e8f251faf13e4fc9aca6ec2e
pranavkale714 <96164632+pranavkale714@users.noreply.github.com>
2025-11-28 11:22:12 +0530

    Enhance theory.java with lambda expression details
    Added explanation and examples of lambda expressions.

--- lambada_expression_demo/src/lambada_expression_demo/theory.java
+++ lambada_expression_demo/src/lambada_expression_demo/theory.java
@@ -5,9 +5,16 @@ public class theory {
 	public static void main(String[] args) {
 	// TODO Auto-generated method stub
 	System.out.println("Why Lambada Expression ?");	
-	System.out.println("Quick and Short Function");
-	System.out.println("It can be use only one time");
-   
+	System.out.println("1.Quick and Short Function");
+	System.out.println("2.It can be use only one time");
+
+
+		System.out.println("Note:-It can be use multiple time if you assign it to variable");
+		System.out.println("Example:-");
+		Predicate<Integer> isEven = x -> x % 2 == 0;  // Lambda assigned to a variable
+		System.out.println(isEven.test(10));  // true
+		System.out.println(isEven.test(15));  // false 
 	}
 
 }
+
