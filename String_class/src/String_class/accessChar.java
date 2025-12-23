26d0b2ab871ffb14898023925e814c8dfe5bdc72
pranavkale714 <96164632+pranavkale714@users.noreply.github.com>
2025-12-23 12:29:16 +0530

    Add accessChar class to print characters of a string

---
+++ String_class/src/String_class/accessChar.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class accessChar{
+    
+    public void accessCharacters(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            System.out.println(s.charAt(i));
+        }
+    }
+}
